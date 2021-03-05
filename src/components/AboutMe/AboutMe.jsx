33a4e6963742e41f14d87bdc5a4bae528865ab79
erikacolette <erikatidwell.c@gmail.com>
2021-03-05 15:51:05 -0500

    aboutme component

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./AboutMe.css"
+
+const AboutMe = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="heading" id="about">
+          About Me
+        </h1>
+        <hr className="hr" />
+        <p className="blurb-text">
+          Hello! I'm Erika, a full-stack software engineer based in New York,
+          NY.
+        </p>
+
+        <p className="blurb-text">
+          In 2014, I graduated from{" "}
+          <a className="green" href="https://sva.edu/" target="_blank">
+            The School of Visual Arts
+          </a>{" "}
+          with a BFA in Illustration. In 2016, I graduated from{" "}
+          <a
+            className="green"
+            href="https://www.swedishinstitute.edu/"
+            target="_blank"
+          >
+            The Swedish Institute of Health Sciences
+          </a>{" "}
+          with a AOS in Massage Therapy.
+        </p>
+        <p className="blurb-text">
+          I am a recent grad from{" "}
+          <a
+            className="green"
+            href="https://generalassemb.ly/education/software-engineering-immersive/new-york-city"
+            target="_blank"
+          >
+            General Assembly's{" "}
+          </a>
+          Software Engineering Program. I enjoy building a variety of meaningful
+          and beautiful websites that are relative to my passions.{" "}
+        </p>
+
+        <p className="blurb-text">
+          Here are a few technologies I've been working with recently:
+        </p>
+        <div class="table-responsive">
+          <table class="table table-borderless blurb-text">
+            <tbody>
+              <tr>
+                <td>React</td>
+                <td>Node.js</td>
+                <td>Javascript</td>
+              </tr>
+              <tr>
+                <td>Html</td>
+                <td>CSS</td>
+                <td>Python</td>
+              </tr>
+              <tr>
+                <td>Bootstrap</td>
+                <td>MongoDB</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <img className="profile-pic" src="/images/erika.jpeg" alt="erika-pic" />
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import AboutMe from "../../components/AboutMe/AboutMe"
 import "./HomePage.css";
 
 const HomePage = () => {
@@ -16,60 +17,9 @@ const HomePage = () => {
           Get In Touch
         </a>
       </div>
+      <AboutMe />
 
-      <div className="container">
-        <h1 className="heading" id="about">
-          About Me
-        </h1>
-        <hr className="hr" />
-        <p className="blurb-text">
-          Hello! I'm Erika, a full-stack software engineer based in New York,
-          NY.
-        </p>
-
-        <p className="blurb-text">
-          In 2014, I graduated from{" "}
-          <a className="green" href="https://sva.edu/" target="_blank">
-            The School of Visual Arts
-          </a>{" "}
-          with a BFA in Illustration. In 2016, I graduated from{" "}
-          <a
-            className="green"
-            href="https://www.swedishinstitute.edu/"
-            target="_blank"
-          >
-            The Swedish Institute of Health Sciences
-          </a>{" "}
-          with a AOS in Massage Therapy.
-        </p>
-        <p className="blurb-text">I am a recent grad from <a className="green" href="https://generalassemb.ly/education/software-engineering-immersive/new-york-city" target="_blank">General Assembly's </a>Software Engineering Program. I enjoy building a variety of meaningful and beautiful websites that are relative to my passions. </p>
-
-        <p className="blurb-text">Here are a few technologies I've been working with recently:</p>
-        <div class="table-responsive">
-          <table class="table table-borderless blurb-text">
-            <tbody>
-              <tr>
-                
-                <td>React</td>
-                <td>Node.js</td>
-                <td>Javascript</td>
-              </tr>
-              <tr>
-               
-                <td>Html</td>
-                <td>CSS</td>
-                <td>Python</td>
-              </tr>
-              <tr>
-                
-                <td>Bootstrap</td>
-                <td>MongoDB</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <img className="profile-pic" src="/images/erika.jpeg" alt="erika-pic" />
-      </div>
+      
     </>
   );
 };
