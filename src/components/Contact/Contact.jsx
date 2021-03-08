597e9551db09e5fad9f6886a8af22656829581ae
erikacolette <erikatidwell.c@gmail.com>
2021-03-08 13:40:58 -0500

    width edit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <h1 className="heading" id="contact">Get In Touch</h1>
     <hr className="hr"/>
 
-    <p className=" work-text">Please feel free to contact me anytime! I'm open to new work and fun creative passion projects.</p>
+    <p className=" work-text1">Please feel free to contact me anytime! I'm open to new work and fun creative passion projects.</p>
     <a href="mailto:erikatidwell.c@gmail.com" className="button">Say Hello</a>
     <div className="icons-container">
         <a
